bf3e6f6c3fa456d61c5d6a9696a46899a5f605e4
jachymsmid <jerejere986@gmail.com>
2025-11-28 11:51:36 +0100

    class for data storage

--- utils/DataStorage.hpp
+++ utils/DataStorage.hpp
@@ -1,3 +1,39 @@
+#pragma once
+
 #include "GeneralVector.hpp"
 template < class RealNumber, std::size_t Size, std::size_t Length >
-using DataStorage = std::array< Vector< RealNumber, Length >, Size >;
+class DataStorage
+{
+public:
+
+  //constructor
+  DataStorage() = default;
+
+  // the data being stored
+  std::array< Vector< RealNumber, Length >, Size > data;
+
+  // getSize and getLength methods
+  std::size_t getSize() { return Size; }
+  std::size_t getLength() { return Length; }
+
+  // acces the element at i,j
+  RealNumber operator()(std::size_t i, std::size_t j)
+  {
+    return data[i][j];
+  }
+
+  // acces the cell values at index i
+  Vector< RealNumber, Size > operator()(std::size_t i)
+  {
+    Vector< RealNumber, Size > temp;
+    for ( std::size_t j = 0; j < Size; j++)
+    {
+      temp[j] = data[i][j];
+    }
+    return temp;
+  }
+
+  // print the data into a file
+  void print() {}
+
+};
