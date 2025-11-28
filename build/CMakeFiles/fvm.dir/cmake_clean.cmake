77f177062e13a45a7bf9a7331f6fb6715ce5ad1d
jachymsmid <jerejere986@gmail.com>
2025-11-28 16:43:54 +0100

    cmake

---
+++ build/CMakeFiles/fvm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fvm.dir/link.d"
+  "CMakeFiles/fvm.dir/main.cpp.o"
+  "CMakeFiles/fvm.dir/main.cpp.o.d"
+  "fvm"
+  "fvm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fvm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/fvm.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for fvm.
+# This may be replaced when dependencies are built.
