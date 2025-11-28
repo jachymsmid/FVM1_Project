0ba602de9b146ca1ed696564f8d1cc31caeab8cf
jachymsmid <jerejere986@gmail.com>
2025-11-28 13:18:17 +0100

    utilized the new data structure

--- utils/FVMsolver.hpp
+++ utils/FVMsolver.hpp
@@ -8,55 +8,46 @@
 
 #include <cstdlib>
 #include "GeneralVector.hpp"
-#include "DataStorage.hpp"
 
 template
 <
-    size_t Size,
-    size_t Length,
     class RealNumber,
     template< size_t, class, class > class NumericalFlux,  // numerical flux
     class Equation,  // physical flux F(u)
-    class BoundaryCondition
+    class BoundaryCondition,
+    class DataStorage
 >
 class FVMsolver {
 public:
 
-  using Data = DataStorage< RealNumber, Size, Length >;
     
     // constructor
     FVMsolver( RealNumber dx, Equation Flux, BoundaryCondition BC) : spatial_step_(dx), boundary_condition_(BC) {}
 
-    void rhs( RealNumber t, const Data &data, Data &rhs_array )
+    void rhs( const DataStorage &data, DataStorage &rhs_array )
     {
 
-      Vector< RealNumber, Size > ghost_cell_left = boundary_condition_.left( data );
-      Vector< RealNumber, Size > ghost_cell_right = boundary_condition_.right( data );
+      Vector< RealNumber, data.getSize() > ghost_cell_left = boundary_condition_.left( data );
+      Vector< RealNumber, data.getSize() > ghost_cell_right = boundary_condition_.right( data );
 
-      Vector< RealNumber, Size > value_left, value_center, value_right; // all the values for one cell, so for eulers this is a vector of three values
-      RealNumber interface_flux_l, interface_flux_r;
+      Vector< RealNumber, data.getSize() > value_left, value_center, value_right; // all the values for one cell, so for eulers this is a vector of three values
 
-      for (std::size_t j = 0; j < Length/2; j++)
+      for (std::size_t j = 0; j < data.getLength()/2; j++)
       {
-        for (std::size_t k = 0; k < Size; k++)
-        {
-          value_left[k] = data[k][2*j];
-          value_center[k] = data[k][2*j+1];
-          value_right[k] = data[k][2*j+2];
-        }
+        // get the data for the central, left and right cell
+        value_left = ( j == 0 ) ? ghost_cell_left : data(2*j);
+        value_center = data(2*j+1);
+        value_right = ( j == data.getLength()/2 - 1 ) ? ghost_cell_right : data(2*j+2);
 
-        for ( std::size_t k = 0; k < Size; k++)
-        {
-          interface_flux_[k][2*j] = NumericalFlux< Size, RealNumber, Equation >::numerical_flux( value_left, value_center );
-          interface_flux_[k][2*j+1] = NumericalFlux< Size, RealNumber, Equation >::numerical_flux( value_center, value_right );
-        }
+        interface_flux_(2*j) = NumericalFlux< data.getSize(), RealNumber, Equation >::numerical_flux( value_left, value_center );
+        interface_flux_(2*j+1) = NumericalFlux< data.getSize(), RealNumber, Equation >::numerical_flux( value_center, value_right );
       }
 
-      for ( std::size_t i = 0; i < Size; i++ )
+      for ( std::size_t i = 0; i < data.getSize(); i++ )
       {
-        for ( std::size_t j = 0; j < Length - 1; j++ )
+        for ( std::size_t j = 0; j < data.getLength(); j++ )
         {
-          rhs_array[i][j+1] = - ( interface_flux_[i][j] - interface_flux_[i][j+1] ) / spatial_step_;
+          rhs_array(i,j+1) = - ( interface_flux_(i,j) - interface_flux_(i,j+1) ) / spatial_step_;
         }
       }
     }
@@ -64,7 +55,7 @@ public:
 private:
     RealNumber spatial_step_; // only for regular grids
     BoundaryCondition boundary_condition_;
-    DataStorage< RealNumber, Size, Length - 1 > interface_flux_;
+    DataStorage interface_flux_;
 };
 
 
@@ -86,6 +77,7 @@ struct Rusanov {
     // how to pass this function?
 
     Vector< RealNumber, Size > flux;
+
     for ( size_t i = 0; i < Size; i++ )
     {
       flux[i] = 0.5*( flux_left[i] + flux_right[i]) - 0.5*a*( value_right[i] - value_left[i] );
